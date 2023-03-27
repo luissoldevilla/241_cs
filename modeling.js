2cd7f42c9abca8ebba29f610431977a8696c09ca
luissoldevilla <luis.angelsoto@hotmail.com>
2023-03-27 10:48:51 -0400

    Modeling JS: For loops

--- modeling.js
+++ modeling.js
@@ -1,12 +1,15 @@
 // Making Functions
 // Using Vanilla JS
 
+// increase all the integers in the array by one
+
 function overview(arr) {
     // checking if reaching here
     console.log(arr)
     for (let i = 0; i < arr.length; i++) {
         let index1 = arr[i];
         console.log(index1);
+        index1++
     }
 }
 
