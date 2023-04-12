9f17f217db9ca4be22d3180811603e2020afceb9
luissoldevilla <luis.angelsoto@hotmail.com>
2023-04-12 10:35:42 -0400

    Modeling Using Vanilla JS

--- modeling.js
+++ modeling.js
@@ -8,7 +8,7 @@ function overview(arr) {
     console.log(arr)
     for (let i = 0; i < arr.length; i++) {
         let index1 = arr[i];
-        console.log(arr[i++])
+        console.log(arr[i] + 1)
     }
 }
 
